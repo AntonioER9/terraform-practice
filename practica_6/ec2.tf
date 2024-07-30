e9775b11e4406b2829af4d27cecf868f008a8fd8
AntonioER9 <aespinozareyes9@gmail.com>
2024-07-30 18:18:06 -0400

    add ec2 with key

--- practica_6/ec2.tf
+++ practica_6/ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "public_instance" {
-  ami = "ami-0aa7d40eeae50c9a9"
+  ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.public_subnet.id
+  subnet_id     = aws_subnet.public_subnet.id
+  key_name      = data.aws_key_pair.key.key_name
 }
--- practica_6/vpc.tf
+++ practica_6/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "public_subnet" {
   cidr_block              = var.subnets[0]
   map_public_ip_on_launch = true
   tags = {
-    "Name" = "Public subnet"
+    "Name" = "public_subnet"
   }
 }
 
@@ -18,7 +18,7 @@ resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.vpc_virginia.id
   cidr_block = var.subnets[1]
   tags = {
-    "Name" = "Private subnet"
+    "Name" = "private_subnet"
   }
   depends_on = [aws_subnet.public_subnet]
 }
