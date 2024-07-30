e4819c5e1bd2d68e87211cf47daf1ed13f8d5a60
AntonioER9 <aespinozareyes9@gmail.com>
2024-07-30 15:52:49 -0400

    add map variables

--- practica_6/variables.tf
+++ practica_6/variables.tf
@@ -3,13 +3,24 @@ variable "virginia_cidr" {
   type        = string
 }
 
-variable "public_subnet" {
-  description = "CIDR public subnet"
-  type        = string
+# variable "public_subnet" {
+#   description = "CIDR public subnet"
+#   type        = string
+# }
+
+# variable "private_subnet" {
+#   description = "CIDR private subnet"
+#   type        = string
+# }
+
+variable "subnets" {
+  description = "Subnets list"
+  type        = list(string)
 }
 
-variable "private_subnet" {
-  description = "CIDR private subnet"
-  type        = string
+variable "tags" {
+  description = "Tags del proyecto"
+  type        = map(string)
 }
+
 
