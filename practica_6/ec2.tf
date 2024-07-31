8b3b2cfb3966e25548a6f6cc6890face35d9f772
AntonioER9 <aespinozareyes9@gmail.com>
2024-07-31 18:33:59 -0400

    add some comments with new functionalities

--- practica_6/ec2.tf
+++ practica_6/ec2.tf
@@ -3,4 +3,13 @@ resource "aws_instance" "public_instance" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
   key_name      = data.aws_key_pair.key.key_name
+
+  lifecycle {
+    # create_before_destroy = true
+    # prevent_destroy = true
+    # ignore_changes = [ 
+    #   ami
+    #  ]
+    replace_triggered_by = [ aws_subnet.private_subnet ] # Si sufre un cambio, la subnet privada, vamos a destruir y construir nuevamente la instancia.
+  }
 }
